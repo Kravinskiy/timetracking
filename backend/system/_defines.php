702abaa5896b5bcadd3850724eedaaf45e70634a
Kravinskiy <mark.kravinskiy@gmail.com>
2017-04-21 15:25:26 +0200

    gitignore

--- backend/system/_defines.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-  //SQL Connection Defines
-
-  define("PDO_DATABASE","timetrackings");
-  define("PDO_USER","timetracking");
-  define("PDO_HOST","localhost");
-  define("PDO_PASSWORD","timetracking");
-?>
