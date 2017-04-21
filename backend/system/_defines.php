a08898c088e4df72641d5a85161ce04038b630fa
Kravinskiy <kravinskiy@jatekhost.hu>
2017-04-21 15:27:06 +0200

    defines.php

---
+++ backend/system/_defines.php
@@ -0,0 +1,9 @@
+<?php
+
+  //SQL Connection Defines
+
+  define("PDO_DATABASE","timetracking");
+  define("PDO_USER","timetracking");
+  define("PDO_HOST","localhost");
+  define("PDO_PASSWORD","timetracking");
+?>
